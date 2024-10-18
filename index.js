8db0693ff03b5520795a527c09563cc04d790ca2
Martin Odongo <martinwere14@gmail.com>
2024-10-18 12:59:03 +0300

    learning local storage by building a quote application

---
+++ index.js
@@ -0,0 +1,44 @@
+const quoteInput = document.getElementById("quote").value;
+const authorInput = document.getElementById('author').value
+const addButton = document.getElementById("button");
+const storedQuotesSection = document.getElementById('stored-quotes')
+
+addButton.addEventListener('click', addQuote);
+
+function addQuote(){
+    const quote = quoteInput.value;
+    const author = authorInput.value;
+
+    if(quote&&author){
+        const quoteObject ={
+            quote,
+            author
+        };
+        const storedQuotes = JSON.parse(localStorage.getItem('quotes'))||[];
+        storedQuotes.push(quoteObject);
+        localStorage.setItem("quotes", JSON.stringify(storedQuotes));
+        //Update the stored quotes section in the DOM
+
+        displayStoredQuotes();
+
+        quoteInput.value= "";
+        authorInput.value= "";
+    }else{
+        alert("Please fill out the quote and author fields.")
+    }
+} 
+function displayStoredQuotes(){
+    const storedQuotes = JSON.parse(localStorage.getItem('quotes'))||[];
+
+    storedQuotesSection.innerHTML = "";
+    storedQuotes.forEach(quoteObj =>{
+        const quoteItem= document.createElement('p');
+        quoteItem.textContent = `${quoteObj.quote}-${quoteObj.author}`;
+
+        const removeButton = document.createElement("button");
+        removeButton.addEventListener("click",()=>{
+            removeQuote(quoteObj);
+        });
+        quoteItem.appendChild(removeButton)
+    })
+}  
